ad59badf7c1b2ec13f00094029453874aeeac09f
Victor Mihail Galeriu <86662386+robotzi22@users.noreply.github.com>
2022-08-02 09:54:29 +0300

    Add files via upload

---
+++ main1.js
@@ -0,0 +1,40 @@
+const container = document.getElementById("container");
+
+const gridBttn = document.getElementById("gridBttn");
+
+gridBttn.addEventListener('click', ()=> {
+    let gridSize = prompt("How large do yo want to make the grid?(please input the number of cells you wish to make)");
+    while (gridSize > 100 || gridSize < 2 || isNaN(gridSize)){
+        if(isNaN(gridSize)){
+            gridSize = prompt("Only numbers are supported, please pick a size between 2 and 100!")
+        } 
+        else if(gridSize=== null){
+            break
+        } 
+}
+    makeGrid(rows,cols)
+});
+
+function makeGrid(rows, cols) {
+  container.style.setProperty('--grid-rows', rows);
+  container.style.setProperty('--grid-cols', cols);
+  for (c = 0; c < (rows * cols); c++) {
+    let cell = document.createElement("div");
+    console.log(cell);
+    container.appendChild(cell).className = "grid-item";
+    cell.addEventListener("mouseover",()=>{
+        cell.style.backgroundColor = "black";
+        console.log(cell)
+    })
+  };
+};
+
+function resetGrid(input){
+
+
+}
+
+makeGrid(16, 16);
+
+
+
